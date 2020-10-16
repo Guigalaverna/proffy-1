63c4be970ac49c3e7279ea16026114d2920c00d6
Luiz Henrique <lpansarini@gmail.com>
2020-10-16 12:45:42 -0300

    remove auth provider

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -4,16 +4,13 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import TeacherForm from "./pages/TeacherForm";
 import TeacherList from "./pages/TeacherList";
-import { AuthProvider } from "./contexts/auth";
 
 function Routes() {
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <Route path="/" exact component={Landing} />
-        <Route path="/study" component={TeacherList} />
-        <Route path="/give-classes" component={TeacherForm} />
-      </AuthProvider>
+      <Route path="/" exact component={Landing} />
+      <Route path="/study" component={TeacherList} />
+      <Route path="/give-classes" component={TeacherForm} />
     </BrowserRouter>
   );
 }
